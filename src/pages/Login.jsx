d8b4374a176ad9604666ee0a7e32d37e5a1e4c80
Gerwin Gindoy <gerwingindoy@gmail.com>
2023-08-05 19:44:18 +0800

    login Styling

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,16 @@
 import React from "react";
+import "../styles/Login.css";
+import Logo from "../images/RentWise.png";
 
 const Login = () => {
   return (
-    <div className="login template d-flex justify-content-center align-items-center 100-w 100-h">
-      <div className="40-w p-5 rounded bg-unite border shadow">
-        <form>
-          <h3>Sign In</h3>
+    <div className="login">
+      <div className="form">
+        <div className="logo">
+          <img src={Logo} alt="Rentwise" />
+        </div>
+        <h3>Sign In</h3>
+        <form className="p-4">
           <div className="mb-2">
             <label htmlFor="username">Username</label>
             <input type="username" placeholder="Enter Username" className="form-control" />
@@ -14,16 +19,16 @@ const Login = () => {
             <label htmlFor="password">Password</label>
             <input type="password" placeholder="Enter Password" className="form-control" />
           </div>
-          <div className="d-flex justify-content-end">
+          <div className="d-flex justify-content-end mb-2">
             <input type="checkbox" className="custom-control custom-checkbox" id="check" />
             <label htmlFor="check" className="custom-input-label">
               Remember me
             </label>
           </div>
-          <div className="d-grid">
-            <button className="btn btn-primary">Sign in</button>
+          <div className="d-grid mb-2">
+            <button className="btn">Sign in</button>
           </div>
-          <a className="d-flex justify-content-end">Register here</a>
+          <a className="d-flex justify-content-end mb-2">Register here</a>
         </form>
       </div>
     </div>
