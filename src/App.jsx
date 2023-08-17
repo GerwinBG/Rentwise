bec11e9eb48cacd7766a47711594f9d70f079620
Gerwin Gindoy <gerwingindoy@gmail.com>
2023-08-17 09:17:07 +0800

    setup redux for global state

--- src/App.jsx
+++ src/App.jsx
@@ -9,61 +9,67 @@ import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
 import Dashboard from "./pages/Dashboard";
 import './styles/App.css';
+import { Provider } from "react-redux";
+import store from "./store/store";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={
-          <div className="content">
-            <Navbar />
-            <div className="d-flex contentWrap">
-              <div className="d-none d-lg-block sidebar">
-                <Sidebar />
-              </div>
-              <div className="d-flex justify-content-center align-items-center w-100">
-                <Dashboard />
-              </div>
+    <>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={
+              <div className="content">
+                <Navbar />
+                <div className="d-flex contentWrap">
+                  <div className="d-none d-lg-block sidebar">
+                    <Sidebar />
+                  </div>
+                  <div className="d-flex justify-content-center align-items-center w-100">
+                    <Dashboard />
+                  </div>
 
-            </div>
-          </div>
-        } />
-
-        <Route path="/apartments" element={
-          <>
-            <div className="content">
-              <Navbar />
-              <div className="d-flex contentWrap">
-                <div className="d-none d-lg-block sidebar">
-                  <Sidebar />
-                </div>
-                <div className="d-flex justify-content-center align-items-center w-100 overflow-auto">
-                  <Apartments />
-                </div>
-
-              </div>
-            </div>
-          </>
-        } />
-        <Route path="/tenants" element={
-          <>
-            <div className="content">
-              <Navbar />
-              <div className="d-flex contentWrap">
-                <div className="d-none d-lg-block sidebar">
-                  <Sidebar />
-                </div>
-                <div className="pageRender d-flex justify-content-center align-items-center w-100 overflow-auto">
-                  <Tenants />
                 </div>
               </div>
-            </div>
-          </>
-        } />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
-      </Routes>
-    </BrowserRouter >
+            } />
+
+            <Route path="/apartments" element={
+              <>
+                <div className="content">
+                  <Navbar />
+                  <div className="d-flex contentWrap">
+                    <div className="d-none d-lg-block sidebar">
+                      <Sidebar />
+                    </div>
+                    <div className="d-flex justify-content-center align-items-center w-100 overflow-auto">
+                      <Apartments />
+                    </div>
+
+                  </div>
+                </div>
+              </>
+            } />
+            <Route path="/tenants" element={
+              <>
+                <div className="content">
+                  <Navbar />
+                  <div className="d-flex contentWrap">
+                    <div className="d-none d-lg-block sidebar">
+                      <Sidebar />
+                    </div>
+                    <div className="pageRender d-flex justify-content-center align-items-center w-100 overflow-auto">
+                      <Tenants />
+                    </div>
+                  </div>
+                </div>
+              </>
+            } />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<SignUp />} />
+          </Routes>
+        </BrowserRouter >
+      </Provider>
+    </>
   )
 }
 
